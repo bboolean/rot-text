ebfce70ffa8e6f9554209d78cffc15c4cf55ed29
benjamin <benjamin@localhost.localdomain>
2021-10-25 20:51:50 -0400

    Write tests

--- test/rot_text/core_test.clj
+++ test/rot_text/core_test.clj
@@ -1,7 +1,21 @@
 (ns rot-text.core-test
   (:require [clojure.test :refer :all]
-            [rot-text.core :refer :all]))
+            [rot-text.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest numbers-to-letters-test
+  (testing "numbers-to-letters"
+    (is (=
+         "D"
+         (core/numbers-to-letters (- 4 1))))))
+
+(deftest letters-to-numbers-test
+  (testing "letters-to-numbers"
+    (is (=
+         (- 4 1)
+         (core/letters-to-numbers "D")))))
+
+(deftest rot-letter-test
+  (testing "rot-letter"
+    (is (=
+         "D"
+         (core/rot-letter 3 "A")))))
